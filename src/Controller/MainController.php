8cb9be0f42c9bfbb6ba4e61c9529ae82b26adaf8
mikhawa <michaeljpitz@gmail.com>
2025-04-22 09:31:46 +0200

    api/starships

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,4 +15,29 @@ final class MainController extends AbstractController
             'controller_name' => 'MainController',
         ]);
     }
+    #[Route('/api/starships')]
+    public function getCollection(): Response
+    {
+        $starships = [
+            [
+                'name' => 'USS LeafyCruiser (NCC-0001)',
+                'class' => 'Garden',
+                'captain' => 'Jean-Luc Pickles',
+                'status' => 'taken over by Q',
+            ],
+            [
+                'name' => 'USS Espresso (NCC-1234-C)',
+                'class' => 'Latte',
+                'captain' => 'James T. Quick!',
+                'status' => 'repaired',
+            ],
+            [
+                'name' => 'USS Wanderlust (NCC-2024-W)',
+                'class' => 'Delta Tourist',
+                'captain' => 'Kathryn Journeyway',
+                'status' => 'under construction',
+            ],
+        ];
+        return $this->json($starships);
+    }
 }
